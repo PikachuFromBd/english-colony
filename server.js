5e543ac2d595d88acfc10777248177a5f756e6b6
English Colony Developer <developer@example.com>
2026-01-23 15:48:44 +0600

    Revert "Remove Telegram Bot (switching to CDN subdomain for large files)"
    This reverts commit 455a47066025895b5643afc0bc3ac23ebadf098b.

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
+const { initBot } = require('./src/lib/bot') // Import Bot
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = process.env.PORT || 3000
-// when using middleware `hostname` and `port` must be provided below
+
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
+    // Start the Telegram Bot
+    if (process.env.TELEGRAM_BOT_TOKEN) {
+        try {
+            initBot();
+        } catch (e) {
+            console.error('Failed to start Telegram Bot:', e);
+        }
+    }
+
     createServer(async (req, res) => {
         try {
-            // Be sure to pass `true` as the second argument to `url.parse`.
-            // This tells it to parse the query portion of the URL.
             const parsedUrl = parse(req.url, true)
-
             await handle(req, res, parsedUrl)
         } catch (err) {
             console.error('Error occurred handling', req.url, err)
